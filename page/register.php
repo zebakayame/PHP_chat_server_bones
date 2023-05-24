e9687c6fc629a6317b88682dd0fc7db75c89cd7c
Zebaka Yame <98798444+zebakayame@users.noreply.github.com>
2023-05-24 14:35:11 +0200

    file upload type

--- page/register.php
+++ page/register.php
@@ -16,7 +16,7 @@
             <input type="text" placeholder="Enter your new user name" name="user" id="user"><br>
             <input type="password" placeholder="Enter your new Password" name="pass" id="pass"><br>
             <p>Choose your image profile</p>
-            <input type="file" name="fileToUpload" id="fileToUpload"><br>
+            <input type="file" accept=".png, .jpg, .jpegq" name="fileToUpload" id="fileToUpload"><br>
             <button id="registerButton" type="submit">Register</button>
         </form>
     </div>
