6d7d3a5f2cf7d7960fb7bd5a17e2aa16ea601973
zebakayame <mehdezeaiter@outlook.com>
2023-05-26 18:12:00 +0200

    HHH

--- page/settings.php
+++ page/settings.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/settings.css">
     <title>Account Settings</title>
 </head>
 <body>
@@ -12,24 +13,29 @@
         include("sqlConnection.php");
         
         if(isset($_FILES['image'])){
-            echo "true";
-            $path = "../img/pp/";
-            $image_name = $_FILES["image"]["name"];
-            $file_tmp = $_FILES['image']['tmp_name'];
+            $imageName = $_FILES['image']['name'];
+            $imageTmpFile = $_FILES['image']['tmp_name'];
+            $imageType = $_FILES['image']['type'];
 
-            move_uploaded_file($file_tmp,$path . $file_name);
+            $newLocation = "../img/pp/" . $imageName;
+            move_uploaded_file($imageTmpFile, $newLocation);
+
+            $sql = "UPDATE USERS SET image_profile = '$newLocation' WHERE USERS.id = " . $_SESSION['YOURid'];
+            mysqli_query($con, $sql);
+            echo "pp updated succesfully";
+            session_destroy();
+            header("LOCATION: ../index.html");
         }
     ?>
     <h1>User Settings</h1>
-    <h3>Name: <?php echo $_SESSION['username'];?></h3>
-    <h3>User Id (aka: #account created): <?php echo $_SESSION['YOURid'];?></h3>
+    <h2>Name: <?php echo $_SESSION['username'];?></h2>
+    <h3>User Id: <?php echo $_SESSION['YOURid'];?></h3>
     <h3>Profile picture: </h3>
     <?php echo "<img src=". $_SESSION['image_profile']." class='pp'>"?>
-    <h4>Change Profile pictures:</h4>
-
+    <h4>Want to change the profile picture?</h4>
     <form action="" method="POST" enctype="multipart/form-data">
-         <input type="file" name="image" />
-         <input type="submit"/>
+         <p>Select new Profile Image: </p><input type="file" name="image"/>
+         <input type="submit" value="Send new Profile"/>
     </form>
 
 </body>
